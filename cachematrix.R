b2dada5bde191a4663d02218b9ee726d335fcd5e
vvarik <vvarik.mail@gmail.com>
2015-11-17 18:14:49 +0100

    first solution to assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set up inverse matrix storage and retrieval in 
+## cache during first and subsequent calls, respectively
 
-## Write a short comment describing this function
-
+## Creates a list of functions necessary to store and retrieve
+## inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
+        m <- matrix()
+        set <- function(y) {
+                x <<- y
+                m <<- matrix()
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Returns an inverse matrix on first call, using function 'solve'
+## Retrieves inverse matrix from cache during subsequent calls
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(all(!is.na(m))) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
